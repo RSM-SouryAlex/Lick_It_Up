96f761272c91de44da04150a5b9c3df9c0bd9096
RSM-SouryAlex <alex.soury@rsmus.com>
2021-08-20 14:59:56 -0700

    Create Office365MFAStatus.ps1

---
+++ Office365MFAStatus.ps1
@@ -0,0 +1,49 @@
+﻿# List MFA status for all accounts in Office 365
+
+$Result=@() 
+$users = Get-MsolUser -All
+
+ForEach($user in $users)
+{
+    #$user = $_
+    $mfaStatus = $user.StrongAuthenticationRequirements.State 
+    $methodTypes = $user.StrongAuthenticationMethods 
+ 
+    if ($mfaStatus -ne $null -or $methodTypes -ne $null)
+    {
+        if($mfaStatus -eq $null)
+        { 
+            $mfaStatus='Enabled (Conditional Access)'
+        }
+
+        $authMethods = $methodTypes.MethodType
+        $defaultAuthMethod = ($methodTypes | Where{$_.IsDefault -eq "True"}).MethodType 
+        $verifyEmail = $user.StrongAuthenticationUserDetails.Email 
+        $phoneNumber = $user.StrongAuthenticationUserDetails.PhoneNumber
+        $alternativePhoneNumber = $user.StrongAuthenticationUserDetails.AlternativePhoneNumber
+    }
+
+    Else
+    {
+        $mfaStatus = "Disabled"
+        $defaultAuthMethod = $null
+        $verifyEmail = $null
+        $phoneNumber = $null
+        $alternativePhoneNumber = $null
+    }
+    
+    $Result += New-Object PSObject -property @{ 
+        UserName = $user.DisplayName
+        UserPrincipalName = $user.UserPrincipalName
+        MFAStatus = $mfaStatus
+        AuthenticationMethods = $authMethods
+        DefaultAuthMethod = $defaultAuthMethod
+        MFAEmail = $verifyEmail
+        PhoneNumber = $phoneNumber
+        AlternativePhoneNumber = $alternativePhoneNumber
+        IsLicensed = $user.islicensed
+        BlockCredential = $user.blockcredential
+        Licenses = $user.licenses
+    }
+}
+$Result | Select UserName,MFAStatus,MFAEmail,IsLicensed,BlockCredential,PhoneNumber,AlternativePhoneNumber,licenses | ft
