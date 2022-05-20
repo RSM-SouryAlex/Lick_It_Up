79811b7a7deeaa2be03df9747db523c2cc217cf7
RSM-SouryAlex <71046201+RSM-SouryAlex@users.noreply.github.com>
2022-05-19 17:10:31 -0700

    Update DKIM_DMARC_SPF_Check.ps1

--- DKIM_DMARC_SPF_Check.ps1
+++ DKIM_DMARC_SPF_Check.ps1
@@ -1,4 +1,4 @@
-﻿$Domain = 'rsmus.com'
+﻿$Domain = 'mygenfcu.org'
 
 $dkim1 = Resolve-DnsName -Type CNAME -Name selector1._domainkey.$Domain | select -ExpandProperty NameHost
 $dkim2 = Resolve-DnsName -Type CNAME -Name selector2._domainkey.$Domain | select -ExpandProperty NameHost
